6acaecb46d33b52a7d5e2bb916e7163a4ce9b124
Carter Canedy <cartercanedy42@gmail.com>
2024-11-18 15:57:33 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# smlog: a lightweight and *very* basic stdout logging implementation
+
+Useful for basic human-readable logging, and not much else
+
+Rather than other logging implementations, this logger is extremely opinionated for the sake of
+simplicity. The lack of choice means that you should be able to call `smlog::Log::init(...)`
+once in main and immediately start logging using `log`'s logging macros, without adding a
+significant amount of bloat to your project.
+
+## Log formatting
+
+Your log statements will look extremely basic:
+
+* `info!(...)` prints `info: ...`
+* `warn!(...)` prints `warning: ...`
+* `error!(...)` prints `error: ...`
+* `debug!(...)` prints `debug: ...`
+* `trace!(...)` prints `trace: ...`
+
+but sometimes, that's all you really need.
+
+## Examples:
+
+### Basic logging
+
+```rs
+use smlog::{log::{info, error}, Log, LevelFilter};
+use log::{info, error};
+
+fn main() {
+    Log::init(LevelFilter::Info);
+
+    info!("Hello, world!");
+    // Outputs `info: Hello, world!`
+
+    error!("Goodbye, world!");
+    // Outputs `error: Goodbye, world!`
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+#![doc = include_str!("../README.md")]
+
+use std::sync::Mutex;
+
+use log::{LevelFilter, Level};
+
+pub use log::{self, trace, debug, info, warn, error};
+
+/// A quick-and-dirty logging implementation
+pub struct Log;
+
+static LOGGER: Log = Log;
+static LOG_LEVEL_FILTER: Mutex<LevelFilter> = Mutex::new(LevelFilter::Off);
+static LOG_IGNORE: Mutex<Vec<String>> = Mutex::new(Vec::new());
+
+/// Ignores the module specified by `target`
+///
+/// # Arguments
+///
+/// * `target` the module to add to module filter
+///    Includes all submodules of `target`
+///
+/// ```rs
+/// use smlog::{Log, LevelFilter, ignore};
+///
+/// fn main() {
+///     // this will disallow logging from any modules within `foo` namespace, e.g. `foo`,
+///     // `foo::bar`, `foo::baz`, etc.
+///     ignore("foo");
+///     Log::init(LevelFilter::Warn);
+///
+///     // ...
+/// }
+/// ```
+pub fn ignore(target: impl AsRef<str>) {
+    LOG_IGNORE.lock().unwrap().push(target.as_ref().into());
+}
+
+/// Explicitly allows the module specified by `target`
+///
+/// # Arguments
+///
+/// * `target` the module remove from module filter
+///
+/// # Example
+///
+/// ```rs
+/// use smlog::{Log, LevelFilter, ignore, allow};
+/// 
+/// fn main() {
+///     ignore("foo");
+///
+///     Log::init(LevelFilter::Warn);
+///
+///     // ...
+///
+///     allow("foo");
+/// }
+/// ```
+pub fn allow(target: impl AsRef<str>) {
+    let mut ign = LOG_IGNORE.lock().unwrap();
+
+    if let Ok(i) = ign.binary_search(&target.as_ref().into()) {
+        ign.remove(i);
+    }
+}
+
+impl Log {
+    /// Initialize logging with the specified severity filter
+    ///
+    /// # Arguments
+    ///
+    /// * `level_filter` the max severity for emitted logs
+    pub fn init(level_filter: LevelFilter) {
+        *LOG_LEVEL_FILTER.lock().unwrap() = level_filter;
+
+        log::set_logger(&LOGGER)
+            .map(|_| log::set_max_level(level_filter))
+            .unwrap();
+    }
+}
+
+impl log::Log for Log {
+    fn log(&self, record: &log::Record) {
+        if !self.enabled(record.metadata()) {
+            return;
+        }
+
+        let pfx = match record.level() {
+            Level::Error => "error",
+            Level::Warn => "warning",
+            Level::Info => "info",
+            Level::Debug => "debug",
+            Level::Trace => "trace",
+        };
+
+        println!("{pfx}: {}", record.args());
+    }
+
+    fn flush(&self) { }
+
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        *LOG_LEVEL_FILTER.lock().unwrap() >= metadata.level()
+            && !LOG_IGNORE.lock().unwrap().iter().fold(false, |disable, ign| disable || metadata.target().starts_with(ign))
+    }
+}
+
